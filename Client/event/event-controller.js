a33d6073d9d0daf9925a60c82974510094a216c2
Unknown <trahax.dev@gmail.com>
2018-02-19 18:33:36 +0000

    Added more frontend improvements
    Added event page too

--- Client/event/event-controller.js
+++ Client/event/event-controller.js
@@ -2,6 +2,8 @@
     angular.module('ticketPriceInterpreter')
     .controller('eventCtrl', ['$scope', '$http', '$state', '$window', 'userSvc', function($scope, $http, $state, $window, userSvc){
         $scope.eventData =[];
+        $scope.startDateTime = [];
+        $scope.endDateTime = [];
 
         function getEventData(){
             var requestUrl = `/events/get-event-by-id?eventId=${$state.params.eventID}`
@@ -14,11 +16,28 @@
             })
         }
 
+        function getDates() {
+            $scope.startDateTime = moment($scope.eventData.StartDateTime).format("dddd, MMMM D YYYY hh:mma Z");
+            $scope.endDateTime = moment($scope.eventData.endDateTime).format("dddd, MMMM D YYYY hh:mma Z");
+            console.log($scope.startDateTime)
+        }
 
+        // function getPresales() {
+        //     // check if there are presales
+        //     if ($scope.eventData.sales[0][0].presales)
+        //     {
+        //         var presales = $scope.eventData.sales[0].presales;
+        //         for (var i = 0; i < presales.length; i++) {
+        //             console.log(presales[i]);
+        //         }
+        //     }
+        // }
        
 
         function init(){
             getEventData();
+            getDates();
+            // getPresales();
         }
         
         init()
--- app.js
+++ app.js
@@ -45,7 +45,6 @@ schedule.scheduleJob('0 0 * * *', () => {
     // might be a problem as new data is streamed as it's checking. Might have to run at different times.
     getPopularEventsModule.getPopularEvents();
 })
-getPopularEventsModule.getPopularEvents();
 
 
 //--/////////////////////--//
